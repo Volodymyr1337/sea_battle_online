964711c9c49e4ff03febc352eee5fa431f16386b
Volodymyr <docentkafedru@gmail.com>
2017-07-28 23:55:26 +0300

    menu item added

---
+++ Assets/Editor/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems : MonoBehaviour
+{
+    [MenuItem("[ Configure ]/Clear PlayerPrefs")]
+    static void ClearPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        Debug.Log("Player prefs removed.");
+    }
+}
+
